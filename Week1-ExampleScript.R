ac06de81de8e97a09b0a576fc7b782f8651309a3
Elise Larsen <eal109@georgetown.edu>
2019-01-10 14:26:35 -0500

    EL: Update  Week 1 activity with journal review intro.
    Update week1 example R script and corrected .csv for journal reiew activity. First commit for week 1 journal review activity.

--- Week1-ExampleScript.R
+++ Week1-ExampleScript.R
@@ -3,7 +3,8 @@
 
 #### OUTLINE
 # 1. Example script for Intro to R, week 1 activity
-# 2. Instructions for how to use this file in Intro to Github
+# 2. Intro to Journal Review of Statistical Methods
+# 3. Instructions for how to use this file in Intro to Github
 
 # 1. Intro to R: Example script
 
@@ -53,7 +54,111 @@ boxplot(Sepal.Length~Species, data=iris[iris$Species!="versicolor",], notch=TRUE
 
 
 
-## 2. Intro to Github: Instructions 
+## 2. Intro to Journal Review of Statistical Methods
+
+##LOAD DATA
+stat.review<-read.csv("StatMethodReview.csv", header=T)
+
+##VIEW DATA
+head(stat.review) #shows column names and first 5 rows
+summary(stat.review) #gives summary statistics by column
+
+#dplyr is a very useful package for manipulating data
+#install.packages("dplyr") ## install if needed
+library(dplyr)
+library(ggplot2)
+
+##LOOKING AT TRENDS OVER TIME
+
+
+##Type of study by year
+
+paper.x.year<-stat.review %>%
+  group_by(Year, Type.of.study) %>%
+  summarize(n())
+names(paper.x.year)[3]<-"n.papers"
+
+## Visualizations
+## Grouped
+#ggplot(paper.x.year, aes(fill=Type.of.study, y=n.papers, x=Year)) + 
+#  geom_bar(position="dodge", stat="identity")
+
+# Stacked
+ggplot(paper.x.year, aes(fill=Type.of.study, y=n.papers, x=Year)) + 
+  geom_bar( stat="identity") +
+  ylab("Number of Papers")
+
+
+# Stacked Percent
+ggplot(paper.x.year, aes(fill=Type.of.study, y=n.papers, x=Year)) + 
+  geom_bar( stat="identity", position="fill") + 
+  ylab("Proportion of Papers")
+
+
+
+#### SUBSET Papers to *Studies*
+
+#Look at the Proportion of studies (observational, field experiment, lab experiment, data compilation study only) that:
+# Use statistics
+
+##FILTER TO "STUDIES"
+studytypes<-c("Observational study","Field experiment","Lab experiment", "Data compilation study")
+
+#Filter Data
+study.review <- stat.review %>%
+  filter(Type.of.study %in% studytypes) %>%
+  select(Year, Type.of.study, No.statistical.test, Correlation, GOF, Regression, Non.linear, Other.model)
+study.review$Type.of.study<-factor(study.review$Type.of.study)
+
+
+#### HOW OFTEN WERE STATISTICAL TESTS PRESENTED IN STUDIES?
+#NA in "No.statistical.test" indicates some statistical test was performed
+study.review$stat.boolean<-"no"
+study.review$stat.boolean[is.na(study.review$No.statistical.test)]<-"yes"
+study.review$stat.boolean<-factor(study.review$stat.boolean)
+
+#summarize data of interest
+study.x.year<-study.review %>%
+  group_by(Year, Type.of.study, stat.boolean) %>%
+  summarize(n())
+names(study.x.year)[4]<-"n.studies"
+
+## Visualizations
+## stacked
+plotnostatsgroup<-ggplot(study.x.year, aes(fill=(stat.boolean), y=n.studies, x=Year)) + 
+  geom_bar(stat="identity") +
+  ylab("Number of Studies")
+
+#Facet by study type
+plotnostatsgroup + facet_grid(Type.of.study ~ .)
+
+# Stacked Percent
+plotnostatsgroupProp<-ggplot(study.x.year, aes(fill=(stat.boolean), y=n.studies, x=Year)) + 
+  geom_bar( stat="identity", position="fill") + 
+  ylab("Proportion of Studies")
+#Facet by study type
+plotnostatsgroupProp + facet_grid(Type.of.study ~ .)
+
+
+
+# And what Type of statistics (Correlation, GOF, Regression, Other)
+
+
+
+### Class Activities:
+#Look at the proportion of regression studies over time that:
+#  act as if all assumptions met
+#  deal with non-normal errors 
+#  use different approached to deal with non-normal errors (Xform, NP, AltDist)
+#  deal with non-independence (mixed model)
+
+
+#####################  END INTRO TO STAT REVIEW ###################
+
+
+
+
+## 3. Intro to Github: Instructions 
 ##    ALL LINES BELOW COMMENTED OUT BUT YOU WILL RUN SOME OF THEM
 ##
 ## AFTER creating Github account, keep Github credentials available. 
